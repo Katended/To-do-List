b996beebab306fe9fc6a29bb21855c9dda379a53
David Katende <katendedavidjoga@gmail.com>
2023-05-11 15:53:52 +0300

    Add add-remove

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './style.css';
-import Icon from './ellipsis-vertical-svgrepo-com.svg';
-import Icon2 from './recycle-svgrepo-com.svg';
-import Icon3 from './enter-svgrepo-com.svg';
+import ToDo from './js/todo.js';
 
-const myIcon2 = new Image();
+/* const myIcon2 = new Image();
 myIcon2.src = Icon2;
 myIcon2.classList.add('recycle-icon');
 const whatToDo = document.querySelector('.what-to-do');
@@ -14,11 +12,11 @@ myIcon3.src = Icon3;
 myIcon3.classList.add('enter-icon');
 const writeTask = document.querySelector('.write-task');
 writeTask.append(myIcon3);
-
-const placeholder = document.querySelector('.placeholder');
+ */
+/* const placeholder = document.querySelector('.placeholder');
 
 const todaysToDoList = [
-  {
+   {
     description: 'Task One',
     completed: false,
     index: 0,
@@ -28,53 +26,16 @@ const todaysToDoList = [
     completed: true,
     index: 1,
   },
-];
+]; */
 
-const createNewTask = (item) => {
-  const element = document.createElement('div');
-  element.classList.add('task');
-  placeholder.append(element);
-
-  // create input element
-  const checkBox = document.createElement('input');
-  checkBox.classList.add('check-box');
-  checkBox.setAttribute('type', 'checkbox');
-  element.appendChild(checkBox);
-
-  // create p tag
-  const taskText = document.createElement('p');
-  taskText.classList.add('p-tag');
-  taskText.innerHTML = item.description;
-  taskText.classList.add('task-text');
-  element.appendChild(taskText);
-
-  /// add icon
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  return element;
-};
-
-const component = () => {
+/* const component = () => {
   todaysToDoList.forEach((item, index) => {
     if (item.index === index) {
       createNewTask(item);
     }
   });
 };
+ */
 
-component();
-
-const todo = document.querySelector('input');
-['click', 'keypress'].forEach((evt) => {
-  todo.addEventListener(evt, (evt) => {
-    if (evt.keyCode === 13) {
-      if (todo.value !== '') {
-        const length = todaysToDoList.length + 1;
-        todaysToDoList.push({ description: todo.value, completed: false, length });
-        component();
-      }
-    }
-  });
-});
+const myToDo = new ToDo();
+myToDo.display();
